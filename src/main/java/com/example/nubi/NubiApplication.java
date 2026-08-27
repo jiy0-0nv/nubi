9ce36a3f946db71f213d0730a704ffec1ba091f0
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-08-27 16:16:49 +0900

    Initial commit

---
+++ src/main/java/com/example/nubi/NubiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NubiApplication.class, args);
+    }
+
+}
