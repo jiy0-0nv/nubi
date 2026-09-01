f6876af9f42f522fbb78c94abc4ff6deccad4e8d
wonnowone <harmonace10@gmail.com>
2026-09-01 15:40:30 +0900

    리뷰 생성 시 방 별점 변화 쿼리 추가

--- backend/src/main/java/com/nubi/domain/bookings/controller/BookingsController.java
+++ backend/src/main/java/com/nubi/domain/bookings/controller/BookingsController.java
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.Path;
-
 @RestController
 @RequestMapping("/api/bookings")
 @RequiredArgsConstructor
